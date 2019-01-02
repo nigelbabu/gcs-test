800c2b1aa7966ed92d5f669cfe4bfec2ff7ca4e3
Nigel Babu <nigelb@redhat.com>
2019-01-02 14:07:14 +0530

    Add a test to create a PVC

--- test/e2e/gcs.go
+++ test/e2e/gcs.go
@@ -8,10 +8,10 @@ import (
 	//. "github.com/onsi/gomega"
 
 	//appsv1 "k8s.io/api/apps/v1"
-	//"k8s.io/api/core/v1"
+	"k8s.io/api/core/v1"
 	storagev1 "k8s.io/api/storage/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
-	//"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
@@ -23,11 +23,13 @@ var _ = Describe("gcs", func() {
 
 	// filled in BeforeEach
 	var c kubernetes.Interface
+	var ns string
 
 	var outputDir string
 
 	BeforeEach(func() {
 		c = f.ClientSet
+		ns = f.Namespace.Name
 
 		var err error
 		outputDir, err = ioutil.TempDir("", "gcs")
@@ -57,30 +59,30 @@ var _ = Describe("gcs", func() {
 			}), "waiting for glusterfs-csi storageclass")
 			framework.Logf("%v", sc)
 
-			//By("creating a pvc")
-			//pvc := &v1.PersistentVolumeClaim{
-			//ObjectMeta: metav1.ObjectMeta{
-			//GenerateName: "pvc-",
-			//},
-			//Spec: v1.PersistentVolumeClaimSpec{
-			//AccessModes: []v1.PersistentVolumeAccessMode{
-			//v1.ReadWriteMany,
-			//},
-			//Resources: v1.ResourceRequirements{
-			//Requests: v1.ResourceList{
-			//v1.ResourceName(v1.ResourceStorage): resource.MustParse("1Gi"),
-			//},
-			//},
-			//StorageClassName: &scName,
-			//},
-			//}
-			//var err error
-			//pvc, err = c.CoreV1().PersistentVolumeClaims(ns).Create(pvc)
-			//framework.ExpectNoError(err)
+			By("creating a pvc")
+			pvc := &v1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{
+					GenerateName: "pvc-",
+				},
+				Spec: v1.PersistentVolumeClaimSpec{
+					AccessModes: []v1.PersistentVolumeAccessMode{
+						v1.ReadWriteMany,
+					},
+					Resources: v1.ResourceRequirements{
+						Requests: v1.ResourceList{
+							v1.ResourceName(v1.ResourceStorage): resource.MustParse("1Gi"),
+						},
+					},
+					StorageClassName: &scName,
+				},
+			}
+			var err error
+			pvc, err = c.CoreV1().PersistentVolumeClaims(ns).Create(pvc)
+			framework.ExpectNoError(err)
 
-			//By("waiting for pvc to have an efs pv provisioned for it")
-			//framework.ExpectNoError(framework.WaitForPersistentVolumeClaimPhase(v1.ClaimBound, c, ns, pvc.Name, framework.Poll, 1*time.Minute))
-			//defer framework.ExpectNoError(framework.DeletePersistentVolumeClaim(c, pvc.Name, ns))
+			By("waiting for pvc to have an gluster volume pv provisioned for it")
+			framework.ExpectNoError(framework.WaitForPersistentVolumeClaimPhase(v1.ClaimBound, c, ns, pvc.Name, framework.Poll, 1*time.Minute))
+			defer framework.ExpectNoError(framework.DeletePersistentVolumeClaim(c, pvc.Name, ns))
 		})
 	})
 })
